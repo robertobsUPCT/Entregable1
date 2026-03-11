e28c7d7c4c6a4fd742fb0a0630db82c493aaf88b
robertobsUPCT <roberto.bermudez@edu.upct.es>
2026-03-11 18:27:11 +0100

    primeras funcionalidades a Comandante, Opearario, Almacen y Repuesto

--- entregable1.py
+++ entregable1.py
@@ -10,13 +10,12 @@ class Nave(Unidad_Combate):
     def __init__(self, nombre:str, id_combate:str, c_transmision:int):
         super().__init__(id_combate,c_transmision)
         self.nombre = nombre
-        self.piezas_respuesto = list(str)
+        self.piezas_repuesto = []
 
 class Ubicacion(Enum):
     Endor = 1
     Cúmulo_Raimos = 2
-    Nebulosa = 3
-    Kaliida = 4
+    Nebulosa_Kaliida = 3
 
 class Clase(Enum):
     Ejecutor = 1
@@ -25,46 +24,108 @@ class Clase(Enum):
 
 class Estacion_Espacial(Nave):
     def __init__(self, nombre:str, id_combate:str, c_transmision:int, tripulacion:int, pasaje:int, ubicacion:Ubicacion):
-        super().__init__(nombre)
+        super().__init__(nombre,id_combate,c_transmision)
         self.tripulacion = tripulacion
         self.pasaje = pasaje
         self.ubicacion = ubicacion
 
 class Nave_Estelar(Nave):
     def __init__(self, nombre:str, id_combate:str, c_transmision:int, tripulacion:int, pasaje:int, clase:Clase):
-        super().__init__(nombre)
+        super().__init__(nombre,id_combate,c_transmision)
         self.tripulacion = tripulacion
         self.pasaje = pasaje
-        self.ubicacion = clase
+        self.clase = clase
 
 class Caza_Estelar(Nave):
     def __init__(self, nombre:str ,id_combate:str, c_transmision:int, dotacion:int):
-        super().__init__(nombre)
+        super().__init__(nombre,id_combate,c_transmision)
         self.dotacion = dotacion
 
 class Repuesto():
-    def __init__(self, nombre:str, proveedor:str, n_disponibles:int, precio:float):
+    def __init__(self, nombre:str, proveedor:str, stock:int, precio:float):
         self.nombre = nombre
         self.proveedor = proveedor
-        self.__n_disponibles = n_disponibles
+        self.__stock = stock
         self.precio = precio
+    
+    def get_disponibles(self) -> int:
+        return self.__stock
+
+    # def set_disponibles(self, nueva_cantidad: int):
+    #     if nueva_cantidad >= 0:
+    #         self.__stock = nueva_cantidad
+    #     else:
+    #         print("Error: El stock no puede ser negativo")
+
+    def agregar_stock(self,n):
+        self.__stock += n
+
+    def retirar_stock(self,n):
+        if (self.__stock-n)>=0:
+            self.__stock -= n
+        else:
+            print("no se actulizo el stock porque seria negativo")
+    
+    def __str__(self) -> str:
+        return f"Repuesto: {self.nombre} | Proveedor: {self.proveedor} | Stock: {self.get_disponibles()} | Precio: {self.precio} cr"
 
 class Almacen():
-    def __init__(self,nombre:str, localizacion:str, catalogo:list[Repuesto]):
+    def __init__(self, nombre:str, localizacion:str, catalogo:list[Repuesto] = None):
         self.nombre = nombre
         self.localizacion = localizacion
-        self.catalogo = catalogo
+        self.catalogo = catalogo if catalogo is not None else []
+    
+    def buscar_repuesto(self, nom) -> Repuesto:
+        for repuesto in self.catalogo:
+            if repuesto.nombre == nom:
+                return repuesto
+        return None
+    
+    def mostrar_catalogo(self):
+        for repuesto in self.catalogo:
+            print(repuesto)
+
+    def agregar_catalogo(self, repuesto:Repuesto):
+        self.catalogo.append(repuesto)
+
 
 class Usuario():
-    def __init__(self,nombre:str):
-        self.nombre = nombre
+    def __init__(self,usuario:str):
+        self.usuario = usuario
 
 class Comandante(Usuario):
-    def __init__(self,nombre:str,nave:Nave):
-        super.__init__(nombre)
+    def __init__(self,usuario:str,nave:Nave):
+        super().__init__(usuario)
         self.nave = nave
+    
+    def consultar_disponibilidad(self,nombre_repuesto:str,almacen:Almacen):
+        repuesto = almacen.buscar_repuesto(nombre_repuesto)
+
+        if repuesto is not None:
+            print(f"Quedan {repuesto.get_disponibles()} {repuesto.nombre}")
+        else:
+            print(f"el repuesto {nombre_repuesto} no esta disponible en este almacen")
+    
+    def adquirir_repuesto(self, nombre_repuesto:str, almacen:Almacen, n:int):
+        repuesto = almacen.buscar_repuesto(nombre_repuesto)
+
+        if repuesto is not None:
+            pass
 
 class Operario(Usuario):
-    def __init__(self,nombre,almacen:Almacen):
-        super.__init__(nombre)
+    def __init__(self,usuario,almacen:Almacen):
+        super().__init__(usuario)
         self.almacen = almacen
+    
+    def añadir_repuesto(self, repuesto: Repuesto):
+        self.almacen.agregar_catalogo(repuesto)
+    
+    def cambiar_stock(self,nombre_repuesto:str,n:int):
+        repuesto = self.almacen.buscar_repuesto(nombre_repuesto)
+
+        if repuesto is not None:
+            if n > 0:
+                repuesto.agregar_stock(n)
+            else:
+                repuesto.retirar_stock(-n)
+        
